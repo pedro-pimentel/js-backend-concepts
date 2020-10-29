3319b57c7a23abcbe987acd1b16a19a69e409745
Pedro Pimentel <pedropaulosv1@gmail.com>
2020-10-29 18:21:14 -0300

    Commit inicial

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const { response } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.get('/projects', (request, response) => {
+    return response.json({message: 'Hello asd!!'});
+});
+app.post('/projects', (request, response) => {
+    return response.json({
+        message1: 'Hello asd!!',
+        message2: 'Hello asd!!',
+        message3: 'Hello asd!!',
+    });
+});
+app.put('/projects/:id', (request, response) => {
+    return response.json({
+        message1: 'Hello asd!!',
+        message2: 'Hello asd!!',
+        message3: 'Hello asd!!',
+        message4: 'Hello asd!!',
+    });
+});
+app.delete('/projects/:id', (request, response) => {
+    return response.json({
+        message1: 'Hello asd!!',
+        message2: 'Hello asd!!',
+        message3: 'Hello asd!!',
+        message5: 'Hello asd!!',
+    });
+});
+
+app.listen(3333, () => {
+    console.log('back-end started');
+});
